e44cdd4d0620921a5eca7810a4c387de23c43e64
lrodrin <lrodrin@gmail.com>
2019-03-19 22:51:35 +0100

    add server and schema

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# wwcb
+# Flask + GraphQL Example
+This is an example project for using GraphQL (Graphene-SQLAlchemy) with Python-Flask.
+
+## Installing Requirements
+Install the packages.
+```
+pip3 install -r requirements.txt
+```
---
+++ server.py
@@ -0,0 +1,19 @@
+from database import db_session
+from flask import Flask
+from flask_graphql import GraphQLView
+from schema import schema
+
+app = Flask(__name__)
+app.debug = True
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True,
+                                                           context={'session': db_session}))
+
+
+@app.route('/')
+def index():
+    return "Go to /graphql"
+
+
+if __name__ == "__main__":
+    app.run()
