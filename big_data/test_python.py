d0838132479f0ac90a87103d12b63847fd8f4a27
lrodrin <lrodrin@gmail.com>
2024-05-03 12:35:04 +0200

    added mini python test - big data

---
+++ big_data/test_python.py
@@ -0,0 +1,26 @@
+# Question 1: Print "Hello, World!" on the screen
+print("Hello, world!")
+
+# Question 2: Calculate the sum of the numbers 1 to 5
+sum = 0
+for i in range(1, 6):
+    sum += i
+    
+print(sum)
+
+# Question 3: Define a function that returns the square of a num
+def square(num):
+    return num ** 2
+
+print(square(2))
+
+# Question 4: Create a list with the first three pair numbers
+pair_numbers = [2, 4, 6]
+print(pair_numbers)
+
+# Question 5: Use a conditional structure to determine whether a num is positive, negative, or zero
+number = 7
+
+if number > 0: print("Positive")
+elif number < 0: print("Negative")
+else: print("Zero")
